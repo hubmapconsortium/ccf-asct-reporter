e9baa427d9bf8e11040f0867a318d0201c24e8c0
Hrishikesh Paul <hrpaul@iu.edu>
2020-07-28 15:36:29 -0400

    chore: add loading dialog to compare and snackbar

--- src/app/compare/compare.component.ts
+++ src/app/compare/compare.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { FormControl, Validators } from '@angular/forms';
 import { SheetService } from '../services/sheet.service';
+import { LoadingComponent } from '../loading/loading.component';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-compare',
@@ -22,60 +25,75 @@ export class CompareComponent implements OnInit {
 
   @Output() compareData = new EventEmitter<any>();
 
+  loadingDialog: any;
+
   constructor(
     private dialogRef: MatDialogRef<CompareComponent>,
-    public sheet: SheetService
+    public sheet: SheetService,
+    private dialog: MatDialog,
+    public snackBar: MatSnackBar
   ) {}
   ngOnInit(): void {}
 
   public async getData(link: string, columns: string) {
+    this.openLoading();
     let sheetID = this.checkLinkFormat(link).sheetID;
     let gid = this.checkLinkFormat(link).gid;
-    const constructedURL = `https://docs.google.com/spreadsheets/d/${sheetID}/export?format=csv&gid=${gid}`;
-    const CN = columns.split(',');
-    console.log(CN);
-    const markerCol = CN[CN.length - 1];
-    const cellCol = CN[CN.length - 2];
+    try {
+      const constructedURL = `https://docs.google.com/spreadsheets/d/${sheetID}/export?format=csv&gid=${gid}`;
+      const CN = columns.split(',');
 
-    let structureNames = [];
-    const csvData = await this.sheet.getDataFromURL(
-      constructedURL,
-      200,
-      'Ok',
-      1
-    );
+      const markerCol = CN[CN.length - 1];
+      const cellCol = CN[CN.length - 2];
 
-    csvData.data.forEach((row) => {
-      for (let i = 0; i < CN.length - 2; i++) {
-        if (
-          !structureNames.some((s) => s.name.toLowerCase() == row[i].toLowerCase())
-        ) {
-          structureNames.push({name: row[i]});
+      let structureNames = [];
+      const csvData = await this.sheet.getDataFromURL(
+        constructedURL,
+        200,
+        'Ok',
+        1
+      );
+
+      csvData.data.forEach((row) => {
+        for (let i = 0; i < CN.length - 2; i++) {
+          if (
+            !structureNames.some(
+              (s) => s.name.toLowerCase() == row[i].toLowerCase()
+            )
+          ) {
+            structureNames.push({ name: row[i] });
+          }
         }
-      }
-      let cells = row[cellCol].split(',').map((c) => c.trim());
-      for (let i = 0; i < cells.length; i++) {
-        if (
-          !structureNames.some((s) => s.name.toLowerCase() == cells[i].toLowerCase())
-        ) {
-          structureNames.push({name: cells[i].trim()});
+        let cells = row[cellCol].split(',').map((c) => c.trim());
+        for (let i = 0; i < cells.length; i++) {
+          if (
+            !structureNames.some(
+              (s) => s.name.toLowerCase() == cells[i].toLowerCase()
+            )
+          ) {
+            structureNames.push({ name: cells[i].trim() });
+          }
         }
-      }
 
-      let markers = row[markerCol].split(',').map((m) => m.trim());
-      for (let i = 0; i < markers.length; i++) {
-        if (
-          !structureNames.some(
-            (s) => s.name.toLowerCase() == markers[i].toLowerCase()
-          )
-        ) {
-          structureNames.push({name: markers[i]});
+        let markers = row[markerCol].split(',').map((m) => m.trim());
+        for (let i = 0; i < markers.length; i++) {
+          if (
+            !structureNames.some(
+              (s) => s.name.toLowerCase() == markers[i].toLowerCase()
+            )
+          ) {
+            structureNames.push({ name: markers[i] });
+          }
         }
-      }
-    });
+      });
 
-    this.dialogRef.close({data: structureNames});
-    
+      this.openSnackBar('Derived Data sheet succesfully fetched.', 'Close', 'green')
+      this.dialog.closeAll();
+      this.dialogRef.close({ data: structureNames });
+    } catch (err) {
+      this.loadingDialog.close();
+      this.openSnackBar('Error while fetching data.', 'Close', 'red');
+    }
   }
 
   checkLinkFormat(url: string) {
@@ -87,6 +105,22 @@ export class CompareComponent implements OnInit {
         gid: matches[3],
       };
     }
+  }
+
+  openSnackBar(message, action, style) {
+    this.snackBar.open(message, action, {
+      duration: style === 'green' || style === 'warn' ? 3000 : undefined,
+      verticalPosition: 'bottom',
+      horizontalPosition: 'end',
+      panelClass: [`${style}-snackbar`],
+    });
+  }
+
+  openLoading() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    this.loadingDialog = this.dialog.open(LoadingComponent, dialogConfig);
   }
 
   doesFormHaveError() {
