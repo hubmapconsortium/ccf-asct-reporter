2c276398a81e50d9dee7fa8bd9a83e70f3d5ec6c
Hrishikesh Paul <hrpaul@iu.edu>
2020-07-31 11:55:10 -0400

    chore: added node script

---
+++ data-miner/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+var cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/:sheetid/:gid', (req, res) => {
+    var f1 = req.params.sheetid;
+    var f2 = req.params.gid
+    request(`https://docs.google.com/spreadsheets/d/${f1}/export?format=csv&gid=${f2}`, (err, response, body) => {
+        if (err) {
+            res.send({
+                data: [],
+                msg: 'Error from node server',
+                status: 500
+            })
+        }
+        if (response.statusCode == 200) {
+            res.send({
+                data: body,
+                msg: 'Data fetched from node server',
+                status: response.statusCode
+            });
+        } else {
+            res.send({
+                data: [],
+                msg: 'Error from node server',
+                status: response.statusCode
+            });
+        }
+    })
+});
+
+app.listen(3000, () => console.log('Gator app listening on port 3000!'));
