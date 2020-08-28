c2e557a317204ffd76214787bdf8289f0a1b010a
Hrishikesh Paul <hrpaul@iu.edu>
2020-08-28 01:21:12 -0400

    style: lint

--- src/app/tree/vega.config.ts
+++ src/app/tree/vega.config.ts
@@ -6,23 +6,23 @@ import { Marks } from './spec/marks';
 
 export class VegaConfig {
 
-    async makeVegaConfig(currentSheet, bimodalDistance, height, width, treeData, multiParentLinksData) {
-        const config: any = {
-            $schema: 'https://vega.github.io/schema/vega/v5.json',
-            autosize: 'pad',
-            padding: {
-              right: 0,
-              top: 20,
-              bottom: 20,
-              left: 30,
-            },
-            signals: new Signals(),
-            data: new Data(currentSheet, bimodalDistance, height, width, treeData, multiParentLinksData),
-            scales: new Scales(),
-            legends: new Legends(),
-            marks: new Marks(),
-          };
+  async makeVegaConfig(currentSheet, bimodalDistance, height, width, treeData, multiParentLinksData) {
+    const config: any = {
+      $schema: 'https://vega.github.io/schema/vega/v5.json',
+      autosize: 'pad',
+      padding: {
+        right: 0,
+        top: 20,
+        bottom: 20,
+        left: 30,
+      },
+      signals: new Signals(),
+      data: new Data(currentSheet, bimodalDistance, height, width, treeData, multiParentLinksData),
+      scales: new Scales(),
+      legends: new Legends(),
+      marks: new Marks(),
+    };
 
-        return config;
-    }
+    return config;
+  }
 }
