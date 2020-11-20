06409da8f96fc56d88656222a208fa56b835b8c9
Hrishikesh Paul <hrpaul@iu.edu>
2020-11-19 19:22:03 -0500

    test: improved test case

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,12 +3,13 @@ import { AppComponent } from './app.component';
 import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [MatDialogModule, MatSnackBarModule, HttpClientModule],
+      imports: [MatDialogModule, MatSnackBarModule, HttpClientModule, RouterTestingModule],
       declarations: [
         AppComponent
       ],
