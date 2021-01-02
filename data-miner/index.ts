02c663be316ef9aed8276ea8e1a25d4873b06a07
Hrishikesh Paul <hrpaul@iu.edu>
2021-01-02 14:34:36 -0500

    chore: updated table reading

--- data-miner/index.ts
+++ data-miner/index.ts
@@ -16,78 +16,72 @@ app.use(express.static(path.join(__dirname, '/')));
 interface IObjectKeys {
   [key: string]: string;
 }
-interface Structure {
+
+enum BM_TYPE {
+  G = 'gene',
+  P = 'protein'
+}
+
+class Structure {
   name?: string;
   id?: string;
   rdfs_label?: string;
-}
+  b_type?: BM_TYPE;
 
-interface RowI {
-  organ?: Structure;
-  anatomical_structures?: Array<Structure>;
-  tissue_type?: Structure;
-  cell_type?: Structure;
-  biomarkers?: Array<Structure>;
+  constructor() {
+    this.id = '';
+    this.rdfs_label = '';
+    this.name = '';
+  }
 }
 
 class Row {
-  organ: Structure;
   anatomical_structures: Array<Structure>;
-  tissue_type: Structure;
-  cell_type: Structure;
+  cell_types: Array<Structure>;
   biomarkers: Array<Structure>;
 
   constructor() {
-    this.organ = {}
     this.anatomical_structures = []
-    this.tissue_type = {}
-    this.cell_type = {}
+    this.cell_types = []
     this.biomarkers = []
   }
 }
 
 let  headerMap: any = {
-  'Organ Name':'organ', 'Anatomical Structure':'anatomical_structures', 'Tissue Type': 'tissue_type', 'Cell Type': 'cell_type', 'Biomarker': 'biomarkers'}
+ 'AS':'anatomical_structures', 'CT': 'cell_types', 'BG': 'biomarkers', 'BP': 'biomarkers'
+}
 
 let ids = '0123456789'
 
-app.get("/new/:sheetid/:gid", async (req:any, res:any) => {
-  var f1 = req.params.sheetid;
-  var f2 = req.params.gid;
+app.get("/v2/:sheetid/:gid", async (req:any, res:any) => {
+  let f1 = req.params.sheetid;
+  let f2 = req.params.gid;
+  let rows = [];
 
-  
-  
   try {
-    const response = await axios.get(`https://docs.google.com/spreadsheets/d/${f1}/export?format=csv&gid=${f2}`)
-    var data = papa.parse(response.data).data
-    var headerRow = 11
-    var headerLength = data[headerRow].length 
-    var dataLength = data.length - headerRow
+    const response = await axios.get(`https://docs.google.com/spreadsheets/d/${f1}/export?format=csv&gid=${f2}`);
+    let data = papa.parse(response.data).data
+    let headerRow = 11
+    let dataLength = data.length
   
     
-    var rows = [];
-    for (var i = headerRow ; i < dataLength + headerRow; i ++ ) {
+    
+
+    for (let i = headerRow ; i < dataLength; i ++ ) {
       let newRow: {[key: string]: any} = new Row()
-      for (var j = 0 ; j < data[0].length; j ++) {
+
+      for (let j = 0 ; j < data[0].length; j ++) {
         if (data[i][j] === '') continue;
   
         let rowHeader = data[headerRow - 1][j].split('/')
         let key = headerMap[rowHeader[0]]
-        if (rowHeader.length === 1) {
-          newRow[key].name= data[i][j]
-        } 
-        
+
         if (rowHeader.length === 2 && Number(rowHeader[1])) {
           let s: Structure = {name: data[i][j]}
+          if (rowHeader[0] === 'BG') s.b_type = BM_TYPE.G
+          if (rowHeader[0] === 'BP') s.b_type = BM_TYPE.P
           newRow[key].push(s)
         } 
-  
-        if (rowHeader.length === 2 && rowHeader[1] == 'ID') {
-          newRow[key].id = data[i][j]
-        }
-     
-        if (rowHeader.length === 2 && rowHeader[1] == 'LABEL')
-            newRow[key].rdfs_label = data[i][j]
         
   
         if (rowHeader.length === 3 && rowHeader[2] === 'ID') {
@@ -98,20 +92,22 @@ app.get("/new/:sheetid/:gid", async (req:any, res:any) => {
         
       }
       rows.push(newRow)
+      
     }
   } catch(err) {
-    res.status(500).send({
+    console.log(err)
+    return res.status(500).send({
       msg: 'Please check the table format or the sheet access',
       code: 500
     })
   }
   
-  fs.writeFile("../src/app/tree/output.json", JSON.stringify({data: rows}), function(err:any) {
-    if (err) throw err;
-    console.log('complete');
-    }
-);
-  res.send(rows)
+//   fs.writeFile("../src/app/tree/output.json", JSON.stringify({data: rows}), function(err:any) {
+//     if (err) throw err;
+//     console.log('complete');
+//     }
+// );
+  return res.send(rows)
 
 })
 app.get("/", (req:any, res:any) => {
