6abd1c7c29a5f4dd3d2dbf60330cf84a0d0411fc
Hrishikesh Paul <hrpaul@iu.edu>
2021-01-08 23:01:33 -0500

    chore: remove commented code

--- projects/v2/src/app/modules/tree/tree.service.ts
+++ projects/v2/src/app/modules/tree/tree.service.ts
@@ -78,7 +78,6 @@ export class TreeService {
           } else {
             let node = nodes[s];
             if ('isNew' in structure) {
-              // node.isNew = true;
               node.color = structure.color;
               node.pathColor= structure.color;
             }
@@ -105,106 +104,6 @@ export class TreeService {
       this.store.dispatch(new ReportLog(LOG_TYPES.MSG, 'Failed to create Tree', LOG_ICONS.error))
       this.store.dispatch(new HasError(err))
     }
-
-
-
-
-
-
-
-
-    // for (const sheet of compareData) {
-    //   for (const row of sheet.data) {
-    //     data.push(row);
-    //   }
-    // }
-
-    // const linkData = [];
-    // const cols = currentSheet.tree_cols;
-    // const id = 1;
-    // let parent;
-    // const tree = new Tree(id);
-    // const uberon_col = currentSheet.uberon_col;
-
-    // const root = new TNode(id, currentSheet.body, 0, 0, AS_RED);
-    // delete root.parent; delete root.ontology_id;
-    // tree.append(root);
-
-    // try {
-    //   data.forEach(row => {
-    //     parent = root;
-    //     for (const col in cols) {
-
-    //       if (row[cols[col]] === '') {
-    //         continue;
-    //       }
-
-    //       if (row[cols[col]].startsWith('//')) {
-    //         continue;
-    //       }
-
-    //       const foundNodes = row[cols[col]].trim().split();
-    //       for (const i in foundNodes) {
-    //         if (foundNodes[i] !== '') {
-    //           const searchedNode = tree.search(foundNodes[i], parent);
-    //           searchedNode.pathColor = row[row.length - 1];
-
-    //           if (searchedNode.found) {
-    //             if (searchedNode.problem) {
-    //               if (currentSheet.name !== 'ao') {
-    //                 if (!searchedNode.parents.includes(searchedNode.id)) {
-    //                   this.store.dispatch(new ReportLog(LOG_TYPES.MULTI_IN_LINKS, searchedNode.name, LOG_ICONS.warning))
-    //                   // this.report.reportLog(`Nodes with multiple in-links`, 'warning', 'multi', searchedNode.name);
-    //                 }
-    //               }
-    //             }
-    //             parent = searchedNode;
-    //           } else {
-    //             tree.id += 1;
-    //             const uberon =  row[cols[col] + uberon_col] !== foundNodes[i] ? row[cols[col] + uberon_col] : 'NONE';
-    //             const newNode = new TNode(tree.id, foundNodes[i], parent.id, uberon, AS_RED);
-    //             newNode.isNew = row[row.length - 2];
-    //             if (newNode.isNew) {
-    //               newNode.color = row[row.length - 1];
-    //               newNode.pathColor = row[row.length - 1];
-    //             }
-    //             tree.append(newNode);
-    //             parent = newNode;
-    //           }
-    //         }
-    //       }
-    //     }
-    //   });
-    // } catch (err) {
-    //   console.log('Could not process tree data');
-    // }
-
-    // if (tree.nodes.length < 0) {
-    //   console.log('Could not process tree data');
-    // }
-
-    // for (const node of tree.nodes) {
-    //   if (node.problem) {
-    //     for (const p of node.parents) {
-    //       if (p === node.id) {
-    //         this.store.dispatch(new ReportLog(LOG_TYPES.SELF_LINKS, node.name, LOG_ICONS.warning))
-    //         // this.report.reportLog(`Nodes with self-links`, 'warning', 'multi', node.name);
-    //       }
-    //       linkData.push({
-    //         s: p,
-    //         t: node.id
-    //       });
-    //     }
-    //   }
-    // }
-
-    // try {
-    //   const spec = this.vs.makeVegaConfig(currentSheet, currentSheet.config.bimodal_distance, this.height, validateWidth(this.screenWidth, this.controlPaneOpen), tree.nodes, linkData);
-    //   this.store.dispatch(new UpdateVegaSpec(spec));
-    //   this.vs.renderGraph(spec);
-    // } catch(err) {
-    //   console.log(err)
-    // }
   }
 }
 
