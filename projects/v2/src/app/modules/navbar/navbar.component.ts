1a8cfbadebaece64d22def8d11953355e6b7aef4
Hrishikesh Paul <hrpaul@iu.edu>
2021-01-13 16:51:14 -0500

    chore: fixed refresh when in example

--- projects/v2/src/app/modules/navbar/navbar.component.ts
+++ projects/v2/src/app/modules/navbar/navbar.component.ts
@@ -5,7 +5,7 @@ import { SheetState, SheetStateModel } from '../../store/sheet.state';
 import { Observable } from 'rxjs';
 import { Sheet } from '../../models/sheet.model';
 import { Router } from '@angular/router';
-import { FetchSheetData, RefreshData, FetchAllOrganData } from '../../actions/sheet.actions';
+import { FetchSheetData, RefreshData, FetchAllOrganData, FetchInitialPlaygroundData } from '../../actions/sheet.actions';
 import { ToggleControlPane, ToggleIndentList, ToggleReport, ToggleDebugLogs, OpenCompare } from '../../actions/ui.actions';
 import { UIState, UIStateModel } from '../../store/ui.state';
 import { ClearSheetLogs } from '../../actions/logs.actions';
@@ -65,8 +65,13 @@ export class NavbarComponent implements OnInit {
   }
 
   refreshData() {
-    if (this.currentSheet.name === 'all') { this.store.dispatch(new FetchAllOrganData(this.currentSheet)); }
-    else { this.store.dispatch(new FetchSheetData(this.currentSheet)); }
+    if (this.mode === 'vis') {
+      if (this.currentSheet.name === 'all') { this.store.dispatch(new FetchAllOrganData(this.currentSheet)); }
+      else { this.store.dispatch(new FetchSheetData(this.currentSheet)); }
+    } else {
+      this.store.dispatch(new FetchInitialPlaygroundData());
+    }
+    
   }
 
   togglePane() {
