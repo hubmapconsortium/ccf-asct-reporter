35db1f7b09e70444ce7de0ac195e4ed205f84287
Hrishikesh Paul <hrpaul@iu.edu>
2021-02-09 23:52:31 -0500

    chore: improved karma config

--- projects/v2/karma.conf.js
+++ projects/v2/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/v2'),
+      dir: require('path').join(__dirname, './coverage/ccf-asct-reporter'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true,
       thresholds: {
@@ -40,3 +40,4 @@ module.exports = function (config) {
       }
     }
   });
+};
