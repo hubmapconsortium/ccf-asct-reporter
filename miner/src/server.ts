250df769874e740491553b2bcc3cf1b314f71e19
Hrishikesh Paul <hrpaul@iu.edu>
2021-03-08 13:12:33 -0500

    updated

--- miner/src/server.ts
+++ miner/src/server.ts
@@ -3,4 +3,4 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(5000, () => console.log('Example app listening on port 5000!'));
+app.listen(process.env.PORT || 5000, () => console.log('Example app listening on port 5000!'));
