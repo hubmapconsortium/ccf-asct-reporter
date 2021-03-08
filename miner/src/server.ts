789627f901e2171fe2e76a2a1e7cba2f0bf8479e
Hrishikesh Paul <hrpaul@iu.edu>
2021-03-08 13:14:36 -0500

    updated

--- miner/src/server.ts
+++ miner/src/server.ts
@@ -1,6 +1,12 @@
 import express from 'express';
+import cors from 'cors';
+import v1 from './routes/v1.routes';
+import v2 from './routes/v2.routes';
+
 const app = express();
+app.use(cors());
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use('/', v1);
+app.use('/v2', v2);
 
-app.listen(process.env.PORT || 5000, () => console.log('Example app listening on port 5000!'));
+app.listen(process.env.PORT || 5000, () => console.log('ASCT+B Data Miner Started'));
