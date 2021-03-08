359bf57d330c7d447a8ffd42252c2103eeca8272
Hrishikesh Paul <hrpaul@iu.edu>
2021-03-08 13:26:56 -0500

    updated

--- miner/src/routes/v1.routes.ts
+++ miner/src/routes/v1.routes.ts
@@ -1,9 +1,30 @@
 import { Router } from 'express';
+import axios from 'axios';
 
 const v1 = Router();
 
-v1.get('/', (request: any, response: any) => {
-  return response.json("OK V1");
+v1.get("/:sheetid/:gid", async (req: any, res: any) => {
+  var f1 = req.params.sheetid;
+  var f2 = req.params.gid;
+
+  try {
+    const response = await axios.get(`https://docs.google.com/spreadsheets/d/${f1}/export?format=csv&gid=${f2}`)
+    if (response.headers['content-type'] !== 'text/csv') {
+      res.statusMessage = 'Please check if the sheet has the right access'
+      res.status(500).end();
+      return
+    }
+
+    if (response.status === 200) {
+      res.status(206).send(response.data);
+    }
+
+  } catch (err) {
+    console.log(err)
+    res.statusMessage = err
+    res.status(500).end();
+  }
 });
+
 
 export default v1;
