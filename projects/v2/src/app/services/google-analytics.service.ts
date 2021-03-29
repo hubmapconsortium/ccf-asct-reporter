9199d574e94af27b738f5258b431df3f023ee34e
Mac Vogelsang <mac.vogelsang@salesforce.com>
2021-03-29 15:46:25 -0400

    removed models from ga service

--- projects/v2/src/app/services/google-analytics.service.ts
+++ projects/v2/src/app/services/google-analytics.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { GaAction } from '../models/ga.model';
 
 declare let gtag: (arg1?, arg2?, arg3?) => void;
 
@@ -24,23 +25,3 @@ export class GoogleAnalyticsService {
   };
 }
 
-// Standard set of actions for GA events
-export enum GaAction {
-  // Action for Form inputs
-  INPUT = "input", 
-  // Action for toggle ui elements
-  TOGGLE = "toggle",
-  // Action for slider change enents
-  SLIDE = "slide",
-  // Action for other button clicks or graph clicks
-  CLICK = "click",
-  // Action for module navigation
-  NAV = "nav"
-}
-
-export enum GaCategory {
-  NAVBAR = "navbar",
-  GRAPH = "graph",
-  COMPARE = "compare",
-  VISCONTROLS = "viscontrols"
-}
