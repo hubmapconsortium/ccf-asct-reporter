3f68982730ccab3b22d26dcb11ed3c4a61bf6871
Vutukuri Sai Ajay <vutukurisaiajay@Vutukuris-MacBook-Pro.local>
2021-05-28 09:21:22 -0400

    Minor Smell issues

--- projects/v2/src/app/modules/tree/tree.functions.ts
+++ projects/v2/src/app/modules/tree/tree.functions.ts
@@ -1,6 +1,13 @@
-import {AS, CT, B, AS_RED, CT_BLUE, B_GREEN, ST_ID } from '../../models/tree.model';
+import {
+  AS,
+  CT,
+  B,
+  AS_RED,
+  CT_BLUE,
+  B_GREEN,
+  ST_ID,
+} from '../../models/tree.model';
 import { Row } from '../../models/sheet.model';
-
 
 /**
  * Function to compute the Anatomical Structures from the given Data Table.
@@ -13,9 +20,11 @@ export function makeAS(data: Row[]): Array<AS> {
   const anatomicalStructures: Array<AS> = [];
   let id = ST_ID;
   try {
-    data.forEach(row => {
+    data.forEach((row) => {
       row.anatomical_structures.forEach((str, i) => {
-        const foundIndex = anatomicalStructures.findIndex(a => a.comparator === str.name + str.id);
+        const foundIndex = anatomicalStructures.findIndex(
+          (a) => a.comparator === str.name + str.id
+        );
         let newStructure: AS;
         if (foundIndex === -1) {
           newStructure = {
@@ -28,11 +37,15 @@ export function makeAS(data: Row[]): Array<AS> {
             comparator: str.name + str.id,
             label: str.rdfs_label,
             id,
-            organName: row.organName
+            organName: row.organName,
           };
           id += 1;
 
-          if (row.cell_types.length) { newStructure.outdegree.add(`${row.cell_types[0].name}${row.cell_types[0].id}`); }
+          if (row.cell_types.length) {
+            newStructure.outdegree.add(
+              `${row.cell_types[0].name}${row.cell_types[0].id}`
+            );
+          }
           if (i > 0) {
             // needed for the first element to not throw an error
             newStructure.indegree.add(row.anatomical_structures[i - 1].name);
@@ -41,13 +54,16 @@ export function makeAS(data: Row[]): Array<AS> {
           anatomicalStructures.push(newStructure);
         } else {
           if (row.cell_types.length) {
-            anatomicalStructures[foundIndex].outdegree.add(`${row.cell_types[0].name}${row.cell_types[0].id}`);
+            anatomicalStructures[foundIndex].outdegree.add(
+              `${row.cell_types[0].name}${row.cell_types[0].id}`
+            );
           }
           if (i > 0) {
-            anatomicalStructures[foundIndex].indegree.add(row.anatomical_structures[i - 1].name);
+            anatomicalStructures[foundIndex].indegree.add(
+              row.anatomical_structures[i - 1].name
+            );
           }
         }
-
       });
     });
 
@@ -56,7 +72,6 @@ export function makeAS(data: Row[]): Array<AS> {
     throw new Error(`Could not process Anatomical Structures - ${error}`);
   }
 }
-
 
 /**
  * Function to compute the Cell Types from the given Data Table.
@@ -67,9 +82,11 @@ export function makeAS(data: Row[]): Array<AS> {
 export function makeCellTypes(data: Row[]): Array<CT> {
   const cellTypes = [];
   try {
-    data.forEach(row => {
-      row.cell_types.forEach(str => {
-        const foundIndex = cellTypes.findIndex(cell => cell.comparator === (str.name + str.id));
+    data.forEach((row) => {
+      row.cell_types.forEach((str) => {
+        const foundIndex = cellTypes.findIndex(
+          (cell) => cell.comparator === str.name + str.id
+        );
         let newStructure: CT;
         if (foundIndex === -1) {
           newStructure = {
@@ -82,17 +99,21 @@ export function makeCellTypes(data: Row[]): Array<CT> {
             comparator: `${str.name}${str.id}`,
             label: str.rdfs_label,
             references: row.references,
-            organName: row.organName
+            organName: row.organName,
           };
 
           if (row.anatomical_structures.length > 0) {
-            const sn = row.anatomical_structures[row.anatomical_structures.length - 1].name;
-            const sid = row.anatomical_structures[row.anatomical_structures.length - 1].id;
+            const sn =
+              row.anatomical_structures[row.anatomical_structures.length - 1]
+                .name;
+            const sid =
+              row.anatomical_structures[row.anatomical_structures.length - 1]
+                .id;
             newStructure.indegree.add(sn + sid);
           }
 
           // calculate outdegree (CT -> B)
-          row.biomarkers.forEach(marker => {
+          row.biomarkers.forEach((marker) => {
             newStructure.outdegree.add(marker.name + marker.id);
           });
           cellTypes.push(newStructure);
@@ -101,18 +122,18 @@ export function makeCellTypes(data: Row[]): Array<CT> {
             cellTypes[foundIndex].color = str.color;
             cellTypes[foundIndex].pathColor = str.color;
           }
-          row.biomarkers.forEach(marker => {
+          row.biomarkers.forEach((marker) => {
             cellTypes[foundIndex].outdegree.add(marker.name + marker.id);
           });
-          const sn = row.anatomical_structures[row.anatomical_structures.length - 1].name;
-          const sid = row.anatomical_structures[row.anatomical_structures.length - 1].id;
+          const sn =
+            row.anatomical_structures[row.anatomical_structures.length - 1]
+              .name;
+          const sid =
+            row.anatomical_structures[row.anatomical_structures.length - 1].id;
 
           cellTypes[foundIndex].indegree.add(`${sn}${sid}`);
         }
-
-
       });
-
     });
 
     return cellTypes;
@@ -129,19 +150,34 @@ export function makeCellTypes(data: Row[]): Array<CT> {
 export function makeBioMarkers(data: Row[], type?: string): Array<B> {
   const bioMarkers = [];
   try {
-    data.forEach(row => {
+    data.forEach((row) => {
       let currentBiomarkers = [];
       switch (type) {
-        case 'All': currentBiomarkers = row.biomarkers; break;
-        case 'Gene': currentBiomarkers = row.biomarkers_gene; break;
-        case 'Protein': currentBiomarkers = row.biomarkers_protein; break;
-        case 'Lipids': currentBiomarkers = row.biomarkers_lipids; break;
-        case 'Metalloids': currentBiomarkers = row.biomarkers_meta; break;
-        case 'Proteoforms': currentBiomarkers = row.biomarkers_prot; break;
-        default: currentBiomarkers = row.biomarkers;
+        case 'All':
+          currentBiomarkers = row.biomarkers;
+          break;
+        case 'Gene':
+          currentBiomarkers = row.biomarkers_gene;
+          break;
+        case 'Protein':
+          currentBiomarkers = row.biomarkers_protein;
+          break;
+        case 'Lipids':
+          currentBiomarkers = row.biomarkers_lipids;
+          break;
+        case 'Metalloids':
+          currentBiomarkers = row.biomarkers_meta;
+          break;
+        case 'Proteoforms':
+          currentBiomarkers = row.biomarkers_prot;
+          break;
+        default:
+          currentBiomarkers = row.biomarkers;
       }
-      currentBiomarkers.forEach(str => {
-        const foundIndex = bioMarkers.findIndex(i => i.structure === str.name);
+      currentBiomarkers.forEach((str) => {
+        const foundIndex = bioMarkers.findIndex(
+          (i) => i.structure === str.name
+        );
         let newStructure: B;
         if (foundIndex === -1) {
           newStructure = {
@@ -153,11 +189,13 @@ export function makeBioMarkers(data: Row[], type?: string): Array<B> {
             indegree: new Set(),
             nodeSize: 300,
             bType: str.b_type,
-            organName: row.organName
+            organName: row.organName,
           };
 
           if (row.cell_types.length) {
-            newStructure.indegree.add(`${row.cell_types[0].name}${row.cell_types[0].id}`);
+            newStructure.indegree.add(
+              `${row.cell_types[0].name}${row.cell_types[0].id}`
+            );
           }
 
           bioMarkers.push(newStructure);
@@ -167,15 +205,15 @@ export function makeBioMarkers(data: Row[], type?: string): Array<B> {
             bioMarkers[foundIndex].pathColor = str.color;
           }
           if (row.cell_types.length) {
-            bioMarkers[foundIndex].indegree.add(`${row.cell_types[0].name}${row.cell_types[0].id}`);
+            bioMarkers[foundIndex].indegree.add(
+              `${row.cell_types[0].name}${row.cell_types[0].id}`
+            );
           }
         }
       });
     });
     return bioMarkers;
-
   } catch (error) {
     throw new Error(`Could not process Biomarkers - ${error}`);
   }
-
 }
