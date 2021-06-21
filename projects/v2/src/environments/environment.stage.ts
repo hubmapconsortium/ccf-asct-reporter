37355be9b23b465efdfc180e70f7b4bd3c9fa6ed
Vutukuri Sai Ajay <vutukurisaiajay@149-159-201-38.dhcp-bl.indiana.edu>
2021-06-21 11:25:47 -0400

    minor issues

--- projects/v2/src/environments/environment.stage.ts
+++ projects/v2/src/environments/environment.stage.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   tag: 'Staging',
   googleAnalyticsId: 'UA-136932895-4',
-  apasctbApi: 'https://asctb-api--staging.herokuapp.com'
+  asctbApiUrl: 'https://asctb-api--staging.herokuapp.com'
 };
