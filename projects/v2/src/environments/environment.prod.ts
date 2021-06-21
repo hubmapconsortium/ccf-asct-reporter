07a10bad332c346414d73a058f759b0a049bc936
Vutukuri Sai Ajay <vutukurisaiajay@149-159-201-38.dhcp-bl.indiana.edu>
2021-06-21 11:24:54 -0400

    minor issues

--- projects/v2/src/environments/environment.prod.ts
+++ projects/v2/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   tag: '',
   googleAnalyticsId: 'UA-136932895-5',
-  apasctbApi: 'https://asctb-api.herokuapp.com/v2'
+  apasctbApi: 'https://asctb-api.herokuapp.com'
 };
