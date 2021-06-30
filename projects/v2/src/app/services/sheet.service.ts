2b570957dbf62abe2741a19becfe677db5ca735a
vutukuriajay2241 <42532629+vutukuriajay2241@users.noreply.github.com>
2021-06-30 08:32:15 -0400

    Csv  file url can be used in playground component (#171)
    * Postioning all levels of cell types linearly
    
    * Removing commented code
    
    * new column names added
    
    * CSV file can be uploaded in playground component
    
    * Code smells
    
    * Code smells
    
    * suggested changes
    
    * Code smells and minor changes
    
    * minor changes
    
    * minor changes
    
    Co-authored-by: Vutukuri Sai Ajay <vutukurisaiajay@Vutukuris-MacBook-Pro.local>

--- projects/v2/src/app/services/sheet.service.ts
+++ projects/v2/src/app/services/sheet.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { URL, getAssetsURL, getInformation, PLAYGROUND } from './../static/url';
+import { Structure } from '../models/sheet.model';
 
 
 @Injectable({
@@ -54,5 +55,25 @@ export class SheetService {
    */
   updatePlaygroundData(data: string[][]) {
     return this.http.post(`${URL}/v2/playground`, {data});
+  }
+
+  /**
+   * Service to fetch the data for a sheet from CSV file using the api
+   * @param url is the link to the csv file of the sheet
+   */
+   fetchDataFromCSV(url: string) {
+    return this.http.post(`${URL}/v2/csv`, { csvUrl: url });
+  }
+
+  /**
+   * Service to add body for each AS to the data
+   * @param data is the parsed ASCTB data from the csv file of the sheet
+   */
+   getDataWithBody(data: any) {
+    const organ: Structure = { name: 'Body', id: '' };
+    data.forEach((row) => {
+      row.anatomical_structures.unshift(organ);
+    });
+    return data;
   }
 }
