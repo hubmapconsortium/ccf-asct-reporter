85b28f71328367aa1fb441fe8608fb811b013294
Bruce W. Herr II <bherr@indiana.edu>
2021-08-06 14:55:08 -0400

    fix static path

--- asctb-api/src/routes/static-pages.ts
+++ asctb-api/src/routes/static-pages.ts
@@ -4,11 +4,11 @@ import path from 'path';
 export function setupStaticPageRoutes(app: Express): void {
 
   app.get('/graph', (req: Request, res: Response) => {
-    res.sendFile('assets/graph-vis/index.html', { root: path.join(__dirname, '../../') });
+    res.sendFile('assets/graph-vis/index.html', { root: path.join(__dirname, '../../../') });
   });
 
 
   app.get('/', (req: Request, res: Response) => {
-    res.sendFile('views/home.html', { root: path.join(__dirname, '../../') });
+    res.sendFile('views/home.html', { root: path.join(__dirname, '../../../') });
   });
 }
