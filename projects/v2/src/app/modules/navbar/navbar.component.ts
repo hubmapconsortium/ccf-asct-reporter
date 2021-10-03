3eba7bfdc5c527274e496948d04eda5c5c1d63eb
Sai Ajay Vutukuri <vutukuriajay@gmail.com>
2021-10-03 11:41:55 -0400

    Refreshing data when refresh icon is clicked

--- projects/v2/src/app/modules/navbar/navbar.component.ts
+++ projects/v2/src/app/modules/navbar/navbar.component.ts
@@ -5,7 +5,7 @@ import { SheetState, SheetStateModel } from '../../store/sheet.state';
 import { Observable } from 'rxjs';
 import { Sheet, SheetDetails, VersionDetail } from '../../models/sheet.model';
 import { Router } from '@angular/router';
-import { FetchSheetData, FetchAllOrganData } from '../../actions/sheet.actions';
+import { FetchSheetData, FetchAllOrganData, FetchSelectedOrganData } from '../../actions/sheet.actions';
 import { ToggleControlPane, ToggleIndentList, ToggleReport, ToggleDebugLogs, OpenCompare } from '../../actions/ui.actions';
 import { UIState, UIStateModel } from '../../store/ui.state';
 import { ClearSheetLogs } from '../../actions/logs.actions';
@@ -140,6 +140,8 @@ export class NavbarComponent implements OnInit {
   refreshData() {
     if (this.mode === 'vis' && this.currentSheet.name === 'all') {
       this.store.dispatch(new FetchAllOrganData(this.currentSheet));
+    } else if (this.mode === 'vis' && this.currentSheet.name === 'some') {
+      this.store.dispatch(new FetchSelectedOrganData(this.currentSheet, this.selectedOrgans));
     } else {
       this.store.dispatch(new FetchSheetData(this.currentSheet));
     }
