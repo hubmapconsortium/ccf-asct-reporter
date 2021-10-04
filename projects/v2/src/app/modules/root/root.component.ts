08033d346d658efaf18ba9e69c0eb8b7e70f39fd
Nikhil-lab <54205111+Nikhil-lab@users.noreply.github.com>
2021-10-04 14:44:11 -0400

    Feature: JSON-LD export option added to UI (#233)
    * jsonld option added
    
    * JSON-LD option added and worked on review comments
    
    * launch.json removed
    
    * Feature: Multi-selection of versioned organs (#23 #217)
    
    * Documentation for api
    
    * Url title name changed for documentation
    
    * Multi selection organs implemented
    
    * Code smells
    
    * Code smells
    
    * multi selection organs and selected organs count wise export done
    
    * code smells
    
    * code smells
    
    * code smells
    
    * Label change for selected organs
    
    * Identation error in Nav Component
    
    * version support for 1 and 1.1 Draft added
    
    * Removing version on Label
    
    * Label names from the sheet name
    
    Co-authored-by: Vutukuri Sai Ajay <vutukurisaiajay@Vutukuris-MacBook-Pro.local>
    
    * Update ASCTB-TEMP format in JSON-LD export
    
    * One more fix to the IRI
    
    * removing trailing commas
    
    * Graph data in export menu is fixed
    
    * Feature: Display entrez & uniprot links for HGNC terms (#231)
    
    * entrez & uniprot links added
    
    * sonar & lint error fix
    
    * Notes section added to bottom
    
    * Generalizing Extra-Links Object
    
    * reveiw changes
    
    * reveiw changes
    
    * speicific check for ontology removed
    
    * removing commas
    
    Co-authored-by: nmahade <nmahade@iu.edu>
    
    * Refreshing data when refresh icon is clicked
    
    * Removing 1.0 for lung (#238)
    
    * Disabling 1.0 for Lung in organ selector Component.
    
    * Removing 1.0 for lung
    
    * jsonld option added
    
    * JSON-LD option added and worked on review comments
    
    * removing trailing commas
    
    * rebasing
    
    * rebase root component
    
    * JSON-LD error fix
    
    * linting error fix
    
    Co-authored-by: nmahade <nmahade@iu.edu>
    Co-authored-by: vutukuriajay2241 <42532629+vutukuriajay2241@users.noreply.github.com>
    Co-authored-by: Vutukuri Sai Ajay <vutukurisaiajay@Vutukuris-MacBook-Pro.local>
    Co-authored-by: Bruce W. Herr II <bherr@indiana.edu>
    Co-authored-by: Sai Ajay Vutukuri <vutukuriajay@gmail.com>

--- projects/v2/src/app/modules/root/root.component.ts
+++ projects/v2/src/app/modules/root/root.component.ts
@@ -4,7 +4,7 @@ import {
   OnDestroy,
   Output,
   EventEmitter,
-  ViewChild,
+  ViewChild
 } from '@angular/core';
 import { SHEET_CONFIG } from './../../static/config';
 import { SheetState } from './../../store/sheet.state';
@@ -16,8 +16,11 @@ import {
   UpdateReport,
   DeleteCompareSheet,
   UpdateMode,
-  FetchInitialPlaygroundData,
   FetchSelectedOrganData,
+  FetchAllOrganData,
+  FetchSheetData,
+  FetchDataFromAssets,
+  FetchInitialPlaygroundData
 } from './../../actions/sheet.actions';
 import { TreeService } from './../tree/tree.service';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -28,15 +31,12 @@ import {
   CloseRightSideNav,
   CloseCompare,
   CloseLoading,
-  OpenBottomSheet,
+  OpenBottomSheet
 } from '../../actions/ui.actions';
 import { Error } from '../../models/response.model';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { LoadingComponent } from '../../components/loading/loading.component';
-import {
-  MatSnackBar,
-  MatSnackBarConfig,
-} from '@angular/material/snack-bar';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
 import { IndentedListService } from '../../components/indented-list/indented-list.service';
 import { StateReset } from 'ngxs-reset-plugin';
 import { Snackbar } from '../../models/ui.model';
@@ -45,7 +45,7 @@ import { LogsState } from '../../store/logs.state';
 import * as moment from 'moment';
 import {
   MatBottomSheet,
-  MatBottomSheetRef,
+  MatBottomSheetRef
 } from '@angular/material/bottom-sheet';
 import { InfoComponent } from '../../components/info/info.component';
 import { CompareData, DOI, Row, SheetConfig, SheetInfo, VersionDetail } from '../../models/sheet.model';
@@ -110,12 +110,15 @@ export class RootComponent implements OnInit, OnDestroy {
 
   // Sheet Observables
   @Select(SheetState.getData) data$: Observable<any>;
-  @Select(SheetState.getCompareSheets) compareSheets$: Observable<CompareData[]>;
+  @Select(SheetState.getCompareSheets) compareSheets$: Observable<
+    CompareData[]
+  >;
   @Select(SheetState.getReportdata) rd$: Observable<any>;
   @Select(SheetState.getCompareData) compareData$: Observable<Row[]>;
   @Select(SheetState.getAllCompareData) allCompareData$: Observable<any>;
   @Select(SheetState.getMode) mode$: Observable<string>;
-  @Select(SheetState.getBottomSheetInfo) bottomSheetInfo$: Observable<SheetInfo>;
+  @Select(SheetState.getBottomSheetInfo)
+  bottomSheetInfo$: Observable<SheetInfo>;
   @Select(SheetState.getBottomSheetDOI) bottomSheetDOI$: Observable<DOI[]>;
   @Select(SheetState.getSheetConfig) sheetConfig$: Observable<SheetConfig>;
   @Select(SheetState.getFullAsData) fullAsData$: Observable<any>;
@@ -126,7 +129,8 @@ export class RootComponent implements OnInit, OnDestroy {
   @Select(TreeState.getLinksData) links$: Observable<any>;
   @Select(TreeState.getBimodal) bm$: Observable<any>;
   @Select(TreeState.getBiomarkerType) bmType$: Observable<string>;
-  @Select(TreeState.getLatestSearchStructure) searchOption$: Observable<SearchStructure>;
+  @Select(TreeState.getLatestSearchStructure)
+  searchOption$: Observable<SearchStructure>;
 
   // Control Pane Observables
   @Select(UIState.getControlPaneState) pane$: Observable<boolean>;
@@ -158,25 +162,29 @@ export class RootComponent implements OnInit, OnDestroy {
     public sheetService: SheetService,
     public router: Router
   ) {
-
-    this.data$.subscribe(data => {
+    this.data$.subscribe((data) => {
       if (data.length) {
         this.data = data;
         try {
           this.ts.makeTreeData(this.sheet, data, []);
         } catch (err) {
           console.log(err);
-          this.store.dispatch(new HasError({hasError: true, msg: err, status: 400}));
+          this.store.dispatch(
+            new HasError({ hasError: true, msg: err, status: 400 })
+          );
         }
       }
     });
 
-    this.error$.subscribe(err => {
+    this.error$.subscribe((err) => {
       this.error = err.error;
     });
 
-    this.route.queryParamMap.subscribe(query => {
+
+    this.route.queryParamMap.subscribe((query) => {
       const selectedOrgans = query.get('selectedOrgans');
+      const version = query.get('version');
+      const sheet = query.get('sheet');
       const playground = query.get('playground');
       if (!selectedOrgans && playground !== 'true') {
         store.dispatch(new CloseLoading('Select Organ Model Rendered'));
@@ -207,28 +215,44 @@ export class RootComponent implements OnInit, OnDestroy {
       }
       else if (playground === 'true') {
         store.dispatch(new UpdateMode('playground'));
-        this.sheet = SHEET_CONFIG.find(i => i.name === 'example');
+        this.sheet = SHEET_CONFIG.find((i) => i.name === 'example');
         this.store.dispatch(new FetchInitialPlaygroundData());
         store.dispatch(new CloseLoading());
+      } else {
+        store.dispatch(new UpdateMode('vis'));
+        this.sheet = SHEET_CONFIG.find((i) => i.name === sheet);
+        localStorage.setItem('sheet', this.sheet.name);
+        if (version === 'latest') {
+          if (this.sheet.name === 'all') {
+            store.dispatch(new FetchAllOrganData(this.sheet));
+          } else {
+            store.dispatch(new FetchSheetData(this.sheet));
+          }
+        } else {
+          store.dispatch(new FetchDataFromAssets(version, this.sheet));
+        }
       }
     });
 
-    this.loading$.subscribe(l => {
-      if (l && !this.dialog.getDialogById('LoadingDialog')) { this.openLoading(); }
-      else if (!l) { this.closeLoading(); }
+    this.loading$.subscribe((l) => {
+      if (l && !this.dialog.getDialogById('LoadingDialog')) {
+        this.openLoading();
+      } else if (!l) {
+        this.closeLoading();
+      }
     });
 
-    this.uiState$.subscribe(state => {
+    this.uiState$.subscribe((state) => {
       this.isControlPaneOpen = state.controlPaneOpen;
     });
 
-    this.snack$.subscribe(sb => {
-      if (sb.opened)  {
+    this.snack$.subscribe((sb) => {
+      if (sb.opened) {
         const config: MatSnackBarConfig = {
           duration: 1500,
           verticalPosition: 'bottom',
           horizontalPosition: 'end',
-          panelClass: [`${sb.type}-snackbar`]
+          panelClass: [`${sb.type}-snackbar`],
         };
         this.snackbarRef = this.snackbar.open(sb.text, 'Dismiss', config);
         this.snackbarRef.afterDismissed();
@@ -236,30 +260,29 @@ export class RootComponent implements OnInit, OnDestroy {
       }
     });
 
-
-    this.pane$.subscribe(_unused => {
+    this.pane$.subscribe((_unused) => {
       if (this.data) {
         ts.makeTreeData(this.sheet, this.data, []);
       }
     });
 
-    this.bs$.subscribe(value => {
+    this.bs$.subscribe((value) => {
       if (value) {
         this.infoSheetRef = this.infoSheet.open(InfoComponent, {
           disableClose: false,
           hasBackdrop: false,
           autoFocus: false,
           panelClass: 'bottom-sheet-style',
-          data: this.bottomSheetInfo$
+          data: this.bottomSheetInfo$,
         });
-
-
       } else {
-        if (this.infoSheetRef) { this.infoSheetRef.dismiss(); }
+        if (this.infoSheetRef) {
+          this.infoSheetRef.dismiss();
+        }
       }
     });
 
-    this.bottomSheetDOI$.subscribe(data => {
+    this.bottomSheetDOI$.subscribe((data) => {
       if (data.length) {
         this.infoSheetRef = this.infoSheet.open(DoiComponent, {
           disableClose: false,
@@ -276,7 +299,7 @@ export class RootComponent implements OnInit, OnDestroy {
     });
 
     // Listen for changes in the last selected search structure
-    this.searchOption$.subscribe(structure => {
+    this.searchOption$.subscribe((structure) => {
       // Structure will be null when all structures are deselected
       if (this.verticalScrollEntity && structure) {
         const yPos = structure.y + 30; // 30 accounts for top-padding
@@ -284,24 +307,32 @@ export class RootComponent implements OnInit, OnDestroy {
 
         // The vertical scroll div is a CdkScrollable component, but the horizontal scroll element is a normal div.
         // This leads to differences in the scrollTo interface.
-        const contentHeight = this.verticalScrollEntity.treeElementRef.nativeElement.offsetHeight;
-        const contentWidth = this.verticalScrollEntity.treeElementRef.nativeElement.offsetWidth;
-        const yScrollPos = this.verticalScrollEntity.treeElementRef.nativeElement.scrollTop;
-        const xScrollPos = this.verticalScrollEntity.treeElementRef.nativeElement.scrollLeft;
+        const contentHeight =
+          this.verticalScrollEntity.treeElementRef.nativeElement.offsetHeight;
+        const contentWidth =
+          this.verticalScrollEntity.treeElementRef.nativeElement.offsetWidth;
+        const yScrollPos =
+          this.verticalScrollEntity.treeElementRef.nativeElement.scrollTop;
+        const xScrollPos =
+          this.verticalScrollEntity.treeElementRef.nativeElement.scrollLeft;
 
         // Scroll to the selected structure if it's outside the area of the screen
         if (xPos > xScrollPos + contentWidth || xPos < xScrollPos) {
-          this.verticalScrollEntity.treeElementRef.nativeElement.scrollTo(xPos, yScrollPos);
+          this.verticalScrollEntity.treeElementRef.nativeElement.scrollTo(
+            xPos,
+            yScrollPos
+          );
         }
         if (yPos > yScrollPos + contentHeight || yPos < yScrollPos) {
-          this.verticalScrollEntity.treeElementRef.nativeElement.scrollTo({top: yPos - contentHeight / 2});
+          this.verticalScrollEntity.treeElementRef.nativeElement.scrollTo({
+            top: yPos - contentHeight / 2
+          });
         }
       }
     });
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   ngOnDestroy() {
     this.store.dispatch(new StateReset(SheetState));
@@ -325,7 +356,6 @@ export class RootComponent implements OnInit, OnDestroy {
     this.store.dispatch(new DeleteCompareSheet(i));
   }
 
-
   /**
    * Opens loading dialog
    *
@@ -347,7 +377,9 @@ export class RootComponent implements OnInit, OnDestroy {
    */
   closeLoading() {
     const loadingDialog = this.dialog.getDialogById('LoadingDialog');
-    if (loadingDialog) { loadingDialog.close(); }
+    if (loadingDialog) {
+      loadingDialog.close();
+    }
   }
 
   /**
@@ -384,28 +416,29 @@ export class RootComponent implements OnInit, OnDestroy {
     const sn = this.sheet.display.toLowerCase().replace(' ', '_');
     const formatType = option.toLowerCase();
     let csvURL;
-    if (option === 'Graph Data') {
-      const sheet = this.store.selectSnapshot(SheetState.getSheet);
-      const selectedOrgans = this.store.selectSnapshot(SheetState.getSelectedOrgans);
-      const urls = [];
-      if (sheet.name === 'all' || sheet.name === 'some'){
-        for (const organ of selectedOrgans) {
-          SHEET_CONFIG.forEach((config) => {
-            config.version?.forEach((version: VersionDetail) => {
-              if (version.value === organ) {
-                if (version.csvUrl) {
-                  urls.push(version.csvUrl);
-                }
-                else {
-                  urls.push(this.sheetService.formURL(version.sheetId, version.gid));
-                }
+    const sheet = this.store.selectSnapshot(SheetState.getSheet);
+    const selectedOrgans = this.store.selectSnapshot(SheetState.getSelectedOrgans);
+    const urls = [];
+    if (sheet.name === 'all' || sheet.name === 'some'){
+      for (const organ of selectedOrgans) {
+        SHEET_CONFIG.forEach((config) => {
+          config.version?.forEach((version: VersionDetail) => {
+            if (version.value === organ) {
+              if (version.csvUrl) {
+                urls.push(version.csvUrl);
               }
-            });
+              else {
+                urls.push(this.sheetService.formURL(version.sheetId, version.gid));
+              }
+            }
           });
-        }
-        csvURL = urls.join('|');
+        });
       }
-      
+      csvURL = urls.join('|');
+    }
+
+    if (option === 'Graph Data') {
+
       this.sheetService.fetchSheetData(sheet.sheetId, sheet.gid, csvURL? csvURL : sheet.csvUrl, null, 'graph').subscribe((graphData: any) => {
         const graphDataStr = 'data:text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(graphData.data));
         const downloadAnchorNode = document.createElement('a');
@@ -430,6 +463,24 @@ export class RootComponent implements OnInit, OnDestroy {
       document.body.appendChild(downloadAnchorNode);
       downloadAnchorNode.click();
       downloadAnchorNode.remove();
+    } else if (option === 'JSON-LD') {
+
+      this.sheetService
+        .fetchSheetData(sheet.sheetId, sheet.gid, csvURL? csvURL : sheet.csvUrl, null, 'jsonld')
+        .subscribe((graphData: any) => {
+          const graphDataStr =
+            'data:text/json;charset=utf-8,' +
+            encodeURIComponent(JSON.stringify(graphData));
+          const downloadAnchorNode = document.createElement('a');
+          downloadAnchorNode.setAttribute('href', graphDataStr);
+          downloadAnchorNode.setAttribute(
+            'download',
+            `asct+b_json_ld_${sn}_${dt}` + '.json'
+          );
+          document.body.appendChild(downloadAnchorNode);
+          downloadAnchorNode.click();
+          downloadAnchorNode.remove();
+        });
     } else {
       const view = this.store.selectSnapshot(TreeState.getVegaView);
       const fileName = `asct+b_${sn}_${dt}.${formatType}`;
@@ -447,7 +498,5 @@ export class RootComponent implements OnInit, OnDestroy {
           console.log(error);
         });
     }
-
   }
-
 }
