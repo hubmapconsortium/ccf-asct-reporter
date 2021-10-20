aa19dd633b8d92f63c5031bce7872cf4673450e1
nmahade <nmahade@iu.edu>
2021-10-19 23:39:32 -0400

    code smell fix

--- projects/v2/src/app/components/report/report.component.ts
+++ projects/v2/src/app/components/report/report.component.ts
@@ -229,8 +229,8 @@ export class ReportComponent implements OnInit, AfterViewInit {
       if (i < this.reportData.biomarkers.length) {
         row['Unique Biomarkers'] = this.reportData.biomarkers[i].structure;
       }
-      if (i <this.reportData.BWithNoLink.length){
-        row['Biomarkers with no links'] =this.reportData.BWithNoLink[i].structure;
+      if (i < this.reportData.BWithNoLink.length){
+        row['Biomarkers with no links'] = this.reportData.BWithNoLink[i].structure;
       }
       download.push(row);
     }
