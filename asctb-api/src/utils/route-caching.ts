4cbe05d9e82c2410684ae816614c9f34c627901e
Sai Ajay Vutukuri <vutukuriajay@gmail.com>
2021-11-07 12:02:23 -0500

    Caching implemented

--- asctb-api/src/utils/route-caching.ts
+++ asctb-api/src/utils/route-caching.ts
@@ -8,18 +8,23 @@ export function routeCache(duration: number): RequestHandler {
     res.set('Cache-control', `public, max-age=${duration}`);
     const key = '__express__' + req.originalUrl || req.url;
     const cachedBody = mcache.get(key);
-    if (cachedBody) {
-      res.send(cachedBody);
+    // query parameters
+    const cache = req.query.cache as string;
+    if (cache !== 'true') {
+      mcache.del(key);
+      return next();
     } else {
-      if (req.params.cache){
-        const sendResponse = res.send;
-        res.send = (body) => {
-          mcache.put(key, body, duration * 1000);
-          sendResponse.call(res, body);
-          return body;
+      if (cachedBody) {
+        res.send(cachedBody);
+      } else {
+          const sendResponse = res.send;
+          res.send = (body) => {
+            mcache.put(key, body, duration * 1000);
+            sendResponse.call(res, body);
+            return body;
         }
+        next();
       }
-      next();
     }
   };
 }
