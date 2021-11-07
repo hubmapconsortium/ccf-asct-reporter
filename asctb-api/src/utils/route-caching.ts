aec96b80cb5bb0c0fdc7ea01648c53544b265d6a
Sai Ajay Vutukuri <vutukuriajay@gmail.com>
2021-11-07 17:28:18 -0500

    Changes done

--- asctb-api/src/utils/route-caching.ts
+++ asctb-api/src/utils/route-caching.ts
@@ -4,22 +4,26 @@ import mcache from 'memory-cache';
 
 export function routeCache(duration: number): RequestHandler {
   return (req, res, next) => {
-    res.set('Content-Type', 'application/json');
-    res.set('Cache-control', `public, max-age=${duration}`);
-    const key = '__express__' + req.originalUrl || req.url;
-    const cachedBody = mcache.get(key);
     // query parameters
     const cache = req.query.cache as string;
-    if (cachedBody && cache === 'true') {
-      res.send(cachedBody);
-    } else {
-      const sendResponse = res.send;
-      res.send = (body) => {
-        mcache.put(key, body, duration * 1000);
-        sendResponse.call(res, body);
-        return body;
-      };
+    if (cache !== 'true') {
       next();
+    } else {
+      res.set('Content-Type', 'application/json');
+      res.set('Cache-control', `public, max-age=${duration}`);
+      const key = '__express__' + req.originalUrl || req.url;
+      const cachedBody = mcache.get(key);
+      if (cachedBody) {
+        res.send(cachedBody);
+      } else {
+        const sendResponse = res.send;
+        res.send = (body) => {
+          mcache.put(key, body, duration * 1000);
+          sendResponse.call(res, body);
+          return body;
+        };
+        next();
+      }
     }
   };
 }
