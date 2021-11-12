53f5fde114dead927c7cb066851ff8d6183ed463
Edward Lu <36934022+edlu77@users.noreply.github.com>
2021-11-12 17:39:54 -0500

    Fix test

---
+++ projects/v2/src/app/app.component.spec.ts
@@ -0,0 +1,33 @@
+import { MatSnackBar, MatSnackBarRef } from '@angular/material/snack-bar';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ConsentService } from './services/consent.service';
+
+import { Shallow } from 'shallow-render';
+
+import { AppComponent } from './app.component';
+import { AppModule } from './app.module';
+import { RouterModule } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+
+describe('AppComponent', () => {
+  let shallow: Shallow<AppComponent>;
+
+  beforeEach(() => {
+    const mockConsentService = jasmine.createSpyObj<ConsentService>(['setConsent']);
+    shallow = new Shallow(AppComponent, AppModule)
+      .replaceModule(BrowserAnimationsModule, NoopAnimationsModule)
+      .replaceModule(RouterModule, RouterTestingModule.withRoutes([]))
+      .mock(ConsentService, {
+        ...mockConsentService,
+        consent: 'not-set'
+      })
+      .mock(MatSnackBar, {
+        openFromComponent: (): MatSnackBarRef<unknown> => ({} as unknown as MatSnackBarRef<unknown>)
+      });
+  });
+
+  it('should create', async () => {
+    const { instance } = await shallow.render();
+    expect(instance).toBeDefined();
+  });
+});
