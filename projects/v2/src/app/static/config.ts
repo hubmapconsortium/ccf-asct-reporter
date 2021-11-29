35d7adf8a5f132062e2fb7ff146d79194c1e2025
nmahade <nmahade@iu.edu>
2021-11-28 23:58:43 -0500

    sorted sheet-config.json

--- projects/v2/src/app/static/config.ts
+++ projects/v2/src/app/static/config.ts
@@ -869,291 +869,306 @@ export const PLAYGROUND_SHEET_OPTIONS: SheetOptions[] = [
 
 export const SHEET_OPTIONS: SheetOptions[] = [
   {
-    title: 'Bone Marrow',
-    sheet: 'bone_marrow',
-    version: [
-      {
-        value: 'bone_marrow-v1.0',
-        viewValue: 'v1.0',
-      },
-      {
-        value: 'bone_marrow-v1.1-Draft',
-        viewValue: 'v1.1 Draft',
-      },
-    ],
+      'sheet': 'blood',
+      'title': 'Blood',
+      'version': [
+          {
+              'value': 'Blood-v1.1',
+              'viewValue': 'v1.0'
+          }
+      ]
   },
   {
-    title: 'Brain',
-    sheet: 'brain',
-    version: [
-      { value: 'brain-v1.0', viewValue: 'v1.0' },
-      { value: 'brain-v1.1-Draft', viewValue: 'v1.1 Draft' },
-    ],
+      'sheet': 'blood_vasculature',
+      'title': 'Blood Vasculature',
+      'version': [
+          {
+              'value': 'Blood_Vasculature-v1.1',
+              'viewValue': 'v1.0'
+          }
+      ]
   },
   {
-    title: 'Heart',
-    sheet: 'heart',
-    version: [
-      { value: 'heart-v1.0', viewValue: 'v1.0' },
-      { value: 'heart-v1.1-Draft', viewValue: 'v1.1 Draft' },
-    ],
+      'title': 'Bone Marrow',
+      'sheet': 'bone_marrow',
+      'version': [
+          {
+              'value': 'bone_marrow-v1.0',
+              'viewValue': 'v1.0'
+          },
+          {
+              'value': 'bone_marrow-v1.1-Draft',
+              'viewValue': 'v1.1 Draft'
+          }
+      ]
   },
   {
-    title: 'Kidney',
-    sheet: 'kidney',
-    version: [
-      { value: 'kidney-v1.0', viewValue: 'v1.0' },
-      { value: 'kidney-v1.1-Draft', viewValue: 'v1.1 Draft' },
-    ],
+      'title': 'Brain',
+      'sheet': 'brain',
+      'version': [
+          {
+              'value': 'brain-v1.0',
+              'viewValue': 'v1.0'
+          },
+          {
+              'value': 'brain-v1.1-Draft',
+              'viewValue': 'v1.1 Draft'
+          }
+      ]
   },
   {
-    title: 'Large Intestine',
-    sheet: 'large_intestine',
-    version: [
-      { value: 'large_intestine-v1.0', viewValue: 'v1.0' },
-      { value: 'large_intestine-v1.1-Draft', viewValue: 'v1.1 Draft' },
-    ],
-  },
-  // {
-  //   title: 'Liver',
-  //   sheet: 'liver',
-  // },
-  {
-    title: 'Lung',
-    sheet: 'lung',
-    version: [
-      { value: 'lung-v1.1-Draft', viewValue: 'v1.1 Draft' },
-    ],
+      'sheet': 'eye',
+      'title': 'Eye',
+      'version': [
+          {
+              'value': 'Eye-v1.0',
+              'viewValue': 'v1.0'
+          }
+      ]
   },
   {
-    title: 'Lymph Nodes',
-    sheet: 'lymph_nodes',
-    version: [
-      { value: 'lymph_nodes-v1.0', viewValue: 'v1.0' },
-      { value: 'lymph_nodes-v1.1-Draft', viewValue: 'v1.1 Draft' },
-    ],
+      'sheet': 'fallopian_tube',
+      'title': 'Fallopian Tube',
+      'version': [
+          {
+              'value': 'Fallopian_Tube-v1.0',
+              'viewValue': 'v1.0'
+          }
+      ]
   },
   {
-    title: 'Skin',
-    sheet: 'skin',
-    version: [
-      { value: 'skin-v1.0', viewValue: 'v1.0' },
-      { value: 'skin-v1.1-Draft', viewValue: 'v1.1 Draft' },
-    ],
-  },
-  // {
-  //   title: 'Small Intestine',
-  //   sheet: 'small_intestine',
-  // },
-  {
-    title: 'Spleen',
-    sheet: 'spleen',
-    version: [
-      { value: 'spleen-v1.0', viewValue: 'v1.0' },
-      { value: 'spleen-v1.1-Draft', viewValue: 'v1.1 Draft' },
-    ],
+      'title': 'Heart',
+      'sheet': 'heart',
+      'version': [
+          {
+              'value': 'heart-v1.0',
+              'viewValue': 'v1.0'
+          },
+          {
+              'value': 'heart-v1.1-Draft',
+              'viewValue': 'v1.1 Draft'
+          }
+      ]
   },
   {
-    title: 'Thymus',
-    sheet: 'thymus',
-    version: [
-      { value: 'thymus-v1.0', viewValue: 'v1.0' },
-      { value: 'thymus-v1.1-Draft', viewValue: 'v1.1 Draft' },
-    ],
+      'title': 'Kidney',
+      'sheet': 'kidney',
+      'version': [
+          {
+              'value': 'kidney-v1.0',
+              'viewValue': 'v1.0'
+          },
+          {
+              'value': 'kidney-v1.1-Draft',
+              'viewValue': 'v1.1 Draft'
+          }
+      ]
   },
   {
-    title: 'Vasculature',
-    sheet: 'vasculature',
-    version: [
-      { value: 'vasculature-v1.0', viewValue: 'v1.0' },
-      { value: 'vasculature-v1.1-Draft', viewValue: 'v1.1 Draft' },
-    ],
-  },
-  
-
-
-  {
-    sheet: 'eye',
-    title: 'Eye',
-    
-    version: [
-      {
-        value: 'Eye-v1.0',
-        viewValue: 'v1.0',
-      }
-    ],
-
+      'sheet': 'knee',
+      'title': 'Knee',
+      'version': [
+          {
+              'value': 'Knee-v1.0',
+              'viewValue': 'v1.0'
+          }
+      ]
   },
   {
-    sheet: 'fallopian_tube',
-    title: 'Fallopian Tube',
-    
-    version: [
-      {
-        value: 'Fallopian_Tube-v1.0',
-        viewValue: 'v1.0',
-      }
-    ],
-
+      'title': 'Large Intestine',
+      'sheet': 'large_intestine',
+      'version': [
+          {
+              'value': 'large_intestine-v1.0',
+              'viewValue': 'v1.0'
+          },
+          {
+              'value': 'large_intestine-v1.1-Draft',
+              'viewValue': 'v1.1 Draft'
+          }
+      ]
   },
   {
-    sheet: 'knee',
-    title: 'Knee',
-    
-    version: [
-      {
-        value: 'Knee-v1.0',
-        viewValue: 'v1.0',
-      }
-    ],
-
+      'sheet': 'liver',
+      'title': 'Liver',
+      'version': [
+          {
+              'value': 'Liver-v1.0',
+              'viewValue': 'v1.0'
+          }
+      ]
   },
   {
-    sheet: 'liver',
-    title: 'Liver',
-    
-    version: [
-      {
-        value: 'Liver-v1.0',
-        viewValue: 'v1.0',
-      }
-    ],
-
+      'title': 'Lung',
+      'sheet': 'lung',
+      'version': [
+          {
+              'value': 'lung-v1.1-Draft',
+              'viewValue': 'v1.1 Draft'
+          }
+      ]
   },
   {
-    sheet: 'lymph_vasculature',
-    title: 'Lymph Vasculature',
-    
-    version: [
-      {
-        value: 'Lymph_Vasculature-v1.0',
-        viewValue: 'v1.0',
-      }
-    ],
-
+      'title': 'Lymph Nodes',
+      'sheet': 'lymph_nodes',
+      'version': [
+          {
+              'value': 'lymph_nodes-v1.0',
+              'viewValue': 'v1.0'
+          },
+          {
+              'value': 'lymph_nodes-v1.1-Draft',
+              'viewValue': 'v1.1 Draft'
+          }
+      ]
   },
   {
-    sheet: 'pancreas',
-    title: 'Pancreas',
-    
-    version: [
-      {
-        value: 'Pancreas-v1.0',
-        viewValue: 'v1.0',
-      }
-    ],
-
+      'sheet': 'lymph_vasculature',
+      'title': 'Lymph Vasculature',
+      'version': [
+          {
+              'value': 'Lymph_Vasculature-v1.0',
+              'viewValue': 'v1.0'
+          }
+      ]
   },
   {
-    sheet: 'peripheral_nervous_system',
-    title: 'Peripheral Nervous System',
-    
-    version: [
-      {
-        value: 'Peripheral_Nervous_System-v1.0',
-        viewValue: 'v1.0',
-      }
-    ],
-
+      'sheet': 'ovary',
+      'title': 'Ovary',
+      'version': [
+          {
+              'value': 'Ovary-v1.0',
+              'viewValue': 'v1.0'
+          }
+      ]
   },
   {
-    sheet: 'prostate',
-    title: 'Prostate',
-    
-    version: [
-      {
-        value: 'Prostate-v1.0',
-        viewValue: 'v1.0',
-      }
-    ],
-
+      'sheet': 'pancreas',
+      'title': 'Pancreas',
+      'version': [
+          {
+              'value': 'Pancreas-v1.0',
+              'viewValue': 'v1.0'
+          }
+      ]
   },
   {
-    sheet: 'ovary',
-    title: 'Ovary',
-    
-    version: [
-      {
-        value: 'Ovary-v1.0',
-        viewValue: 'v1.0',
-      }
-    ],
-
+      'sheet': 'peripheral_nervous_system',
+      'title': 'Peripheral Nervous System',
+      'version': [
+          {
+              'value': 'Peripheral_Nervous_System-v1.0',
+              'viewValue': 'v1.0'
+          }
+      ]
   },
   {
-    sheet: 'small_intestine',
-    title: 'Small Intestine',
-    
-    version: [
-      {
-        value: 'Small_Intestine-v1.0',
-        viewValue: 'v1.0',
-      }
-    ],
+      'sheet': 'prostate',
+      'title': 'Prostate',
+      'version': [
+          {
+              'value': 'Prostate-v1.0',
+              'viewValue': 'v1.0'
+          }
+      ]
   },
   {
-    sheet: 'ureter',
-    title: 'Ureter',
-    
-    version: [
-      {
-        value: 'Ureter-v1.0',
-        viewValue: 'v1.0',
-      }
-    ],
-
+      'title': 'Skin',
+      'sheet': 'skin',
+      'version': [
+          {
+              'value': 'skin-v1.0',
+              'viewValue': 'v1.0'
+          },
+          {
+              'value': 'skin-v1.1-Draft',
+              'viewValue': 'v1.1 Draft'
+          }
+      ]
   },
   {
-    sheet: 'urinary_bladder',
-    title: 'Urinary Bladder',
-    
-    version: [
-      {
-        value: 'Urinary_Bladder-v1.0',
-        viewValue: 'v1.0',
-      }
-    ],
-
+      'sheet': 'small_intestine',
+      'title': 'Small Intestine',
+      'version': [
+          {
+              'value': 'Small_Intestine-v1.0',
+              'viewValue': 'v1.0'
+          }
+      ]
   },
   {
-    sheet: 'uterus',
-    title: 'Uterus',
-    
-    version: [
-      {
-        value: 'Uterus-v1.0',
-        viewValue: 'v1.0',
-      }
-    ],
-
+      'title': 'Spleen',
+      'sheet': 'spleen',
+      'version': [
+          {
+              'value': 'spleen-v1.0',
+              'viewValue': 'v1.0'
+          },
+          {
+              'value': 'spleen-v1.1-Draft',
+              'viewValue': 'v1.1 Draft'
+          }
+      ]
   },
   {
-    sheet: 'blood',
-    title: 'Blood',
-    
-    version: [
-      {
-        value: 'Blood-v1.1',
-        viewValue: 'v1.0',
-      }
-    ],
-
+      'title': 'Thymus',
+      'sheet': 'thymus',
+      'version': [
+          {
+              'value': 'thymus-v1.0',
+              'viewValue': 'v1.0'
+          },
+          {
+              'value': 'thymus-v1.1-Draft',
+              'viewValue': 'v1.1 Draft'
+          }
+      ]
   },
   {
-    sheet: 'blood_vasculature',
-    title: 'Blood Vasculature',
-    
-    version: [
-      {
-        value: 'Blood_Vasculature-v1.1',
-        viewValue: 'v1.0',
-      }
-    ],
-
+      'sheet': 'ureter',
+      'title': 'Ureter',
+      'version': [
+          {
+              'value': 'Ureter-v1.0',
+              'viewValue': 'v1.0'
+          }
+      ]
+  },
+  {
+      'sheet': 'urinary_bladder',
+      'title': 'Urinary Bladder',
+      'version': [
+          {
+              'value': 'Urinary_Bladder-v1.0',
+              'viewValue': 'v1.0'
+          }
+      ]
+  },
+  {
+      'sheet': 'uterus',
+      'title': 'Uterus',
+      'version': [
+          {
+              'value': 'Uterus-v1.0',
+              'viewValue': 'v1.0'
+          }
+      ]
+  },
+  {
+      'title': 'Vasculature',
+      'sheet': 'vasculature',
+      'version': [
+          {
+              'value': 'vasculature-v1.0',
+              'viewValue': 'v1.0'
+          },
+          {
+              'value': 'vasculature-v1.1-Draft',
+              'viewValue': 'v1.1 Draft'
+          }
+      ]
   }
-
-
-];
+]
 
 export const VERSION = [
   {
