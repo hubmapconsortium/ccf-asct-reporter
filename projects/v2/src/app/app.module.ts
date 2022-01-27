2574e05f4e552d1364f5cb12f3de71074f2bd5aa
Bruce W. Herr II <bherr@indiana.edu>
2022-01-27 10:26:12 -0500

    Update dependencies

--- projects/v2/src/app/app.module.ts
+++ projects/v2/src/app/app.module.ts
@@ -1,40 +1,42 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { APP_INITIALIZER, NgModule } from '@angular/core';
-import { SheetState } from './store/sheet.state';
-import { TreeState } from './store/tree.state';
-import { NgxsModule } from '@ngxs/store';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxsDataPluginModule } from '@ngxs-labs/data';
 import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
-import { NgxsDataPluginModule } from '@ngxs-labs/data';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { NgxsModule } from '@ngxs/store';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { MarkdownModule } from 'ngx-markdown';
+import { NgxsResetPluginModule } from 'ngxs-reset-plugin';
+import { lastValueFrom } from 'rxjs';
 
-import { AnalyticsModule } from './services/analytics.module';
-
+import { environment } from '../environments/environment';
+import { ConfigService } from './app-config.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UIState } from './store/ui.state';
-import { NgxsResetPluginModule } from 'ngxs-reset-plugin';
-import { LogsState } from './store/logs.state';
-import { MarkdownModule } from 'ngx-markdown';
-import { environment } from '../environments/environment';
-import { DocsModule } from './modules/docs/docs.module';
-import { RootModule } from './modules/root/root.module';
-import { HomeModule } from './components/home/home.module';
-import { FooterModule } from './components/footer/footer.module';
 import { FileUploadModule } from './components/file-upload/file-upload.module';
+import { FooterModule } from './components/footer/footer.module';
+import { HomeModule } from './components/home/home.module';
 import { OrganTableSelectorModule } from './components/organ-table-selector/organ-table-selector.module';
 import { TrackingPopupModule } from './components/tracking-popup/tracking-popup.module';
+import { DocsModule } from './modules/docs/docs.module';
+import { RootModule } from './modules/root/root.module';
+import { AnalyticsModule } from './services/analytics.module';
 import { MousePositionTrackerModule } from './services/mouse-position-tracker.module';
-import { ConfigService } from './app-config.service';
+import { LogsState } from './store/logs.state';
+import { SheetState } from './store/sheet.state';
+import { TreeState } from './store/tree.state';
+import { UIState } from './store/ui.state';
+
 
 export function initializeApp(configService: ConfigService): () => Promise<void> {
   return () => Promise.all([
-    configService.sheetConfiguration$.toPromise(),
-    configService.config$.toPromise()
+    lastValueFrom(configService.sheetConfiguration$),
+    lastValueFrom(configService.config$)
   ]).then(() => { });
 }
+
 @NgModule({
   declarations: [
     AppComponent
