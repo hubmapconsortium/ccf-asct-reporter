4ceac3e3214bf35ead748b5f827432c7250b231b
Bruce W. Herr II <bherr@indiana.edu>
2022-01-27 08:50:25 -0500

    Ensure the http request observable is completed after getting the data.

--- projects/v2/src/app/app-config.service.ts
+++ projects/v2/src/app/app-config.service.ts
@@ -2,14 +2,14 @@ import { Injectable }  from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { SheetDetails } from './models/sheet.model';
-import { shareReplay } from 'rxjs/operators';
+import { shareReplay, take } from 'rxjs/operators';
 
 
 @Injectable()
 export class ConfigService {
 
-    sheetConfiguration$ = this.http.get<SheetDetails[]>('assets/sheet-config.json').pipe(shareReplay(1));
-    config$ = this.http.get<Record<string, unknown>>('assets/configuration.json').pipe(shareReplay(1));
- 
+    sheetConfiguration$ = this.http.get<SheetDetails[]>('assets/sheet-config.json').pipe(take(1), shareReplay(1));
+    config$ = this.http.get<Record<string, unknown>>('assets/configuration.json').pipe(take(1), shareReplay(1));
+
     constructor(private readonly http: HttpClient) {}
 }
