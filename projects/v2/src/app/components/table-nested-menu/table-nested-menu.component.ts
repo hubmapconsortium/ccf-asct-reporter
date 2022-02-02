654a3f353c89b773b5fd98d18b59bb82bd5a52f1
Sai Ajay Vutukuri <vutukuriajay@gmail.com>
2022-02-01 23:57:54 -0500

    Nested menu added

---
+++ projects/v2/src/app/components/table-nested-menu/table-nested-menu.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { SheetDetails } from '../../models/sheet.model';
+
+@Component({
+  selector: 'app-table-nested-menu',
+  templateUrl: './table-nested-menu.component.html',
+  styleUrls: ['./table-nested-menu.component.scss']
+})
+export class TableNestedMenuComponent implements OnInit {
+
+  @Input() sheetDetails: SheetDetails[];
+  @Input() title: string;
+  window = window;
+  @ViewChild('childMenu', {static: true}) public childMenu;
+  sheetURL = 'https://docs.google.com/spreadsheets/d/';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  openURL(sheetId: string, gid:string) {
+    this.window.open(`${this.sheetURL}${sheetId}/edit#gid=${gid}`, '_blank');
+  } 
+
+}
