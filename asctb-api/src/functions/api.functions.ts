691082a48a048f14ac17c80852f4651a0cb6e10f
Bruce W. Herr II <bherr@indiana.edu>
2022-04-06 14:18:50 -0400

    Update api.functions.ts

--- asctb-api/src/functions/api.functions.ts
+++ asctb-api/src/functions/api.functions.ts
@@ -4,7 +4,7 @@ import { fixOntologyId } from './lookup.functions';
 
 
 export function normalizeCsvUrl(url: string): string {
-  if (url.startsWith('https://docs.google.com/spreadsheets/d/')) {
+  if (url.startsWith('https://docs.google.com/spreadsheets/d/' && url.indexOf('export?format=csv') === -1) {
     const splitUrl = url.split('/');
     if (splitUrl.length === 7) {
       const sheetId = splitUrl[5];
