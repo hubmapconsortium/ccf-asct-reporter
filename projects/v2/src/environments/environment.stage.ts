55a8c0d19e0ef8b755f483da146fbfed19535bac
Bruce W. Herr II <bherr@indiana.edu>
2022-11-07 15:55:57 -0500

    Switch to AWS endpoint (we'll get the domain name worked out later).

--- projects/v2/src/environments/environment.stage.ts
+++ projects/v2/src/environments/environment.stage.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   tag: 'Staging',
   googleAnalyticsId: 'G-58FWLW86SD',
-  asctbApiUrl: 'https://asctb-api--staging.herokuapp.com'
+  asctbApiUrl: 'https://mmpyikxkcp.us-east-2.awsapprunner.com'
 };
