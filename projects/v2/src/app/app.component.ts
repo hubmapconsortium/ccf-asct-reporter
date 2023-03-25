d8e1fd5da889507d64e8bed024341a6c51d43e75
Pramod <pnbhakta@iu.edu>
2023-03-25 15:34:50 -0400

    linting issues

--- projects/v2/src/app/app.component.ts
+++ projects/v2/src/app/app.component.ts
@@ -24,9 +24,9 @@ export class AppComponent implements OnInit {
     public router: Router) {
 
     switch (environment.tag) {
-      case 'Staging': document.title = 'ASCT+B Reporter | Staging'; break;
-      case 'Development': document.title = 'ASCT+B Reporter | Development'; break;
-      default: document.title = 'ASCT+B Reporter';
+    case 'Staging': document.title = 'ASCT+B Reporter | Staging'; break;
+    case 'Development': document.title = 'ASCT+B Reporter | Development'; break;
+    default: document.title = 'ASCT+B Reporter';
     }
 
     this.matIconRegistry.addSvgIcon(
