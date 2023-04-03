387ae6ffc191def9c311988e5bb2122a2d99d158
Abhay Rajde <abrajde@iu.edu>
2023-04-03 17:55:59 -0400

    updated the source code for generating the summary

--- asctb-api/scripts/generate-draft-validation-reports.js
+++ asctb-api/scripts/generate-draft-validation-reports.js
@@ -29,15 +29,17 @@ async function run() {
 }
 
 async function getData(draftItems) {
-  const headers = ["Validation Tests"];
-  const columns = [["Validation Invalid CSV file?",
-    "Unmapped Metadata found?",
-    "Invalid Header found?",
-    "Missing Header Value found?",
-    "Invalid Character found?",
-    "Missing Uberon or CL IDs?",
-    "Unmapped Data found?",
-    "Bad Column found?"]];
+  const checkmark = '\u2705';
+  const crossmark = '\u274C';
+  const rows = [["Validation Tests","Validation Invalid CSV file?",
+  "Unmapped Metadata found?",
+  "Invalid Header found?",
+  "Missing Header Value found?",
+  "Invalid Character found?",
+  "Missing Uberon or CL IDs?",
+  "Unmapped Data found?",
+  "Bad Column found?"]];
+
   for (let i = 0; i < draftItems.length; i++) {
     const item = draftItems[i];
     const filename = `${item.value}.txt`;
@@ -55,22 +57,15 @@ async function getData(draftItems) {
       const data = await response.data;
       const filetitle = item.value.split('v1.')[0].split(/[_-]/);
       const title = filetitle.join(' ');  
-      headers.push(title.toLowerCase());
-
-      // const validationResults = data.split('\n').slice(0, 8).map((d)=>d.slice(-6));
-      // validationResults.forEach((validationResult) => {
-      //   const result = validationResult === 'passed' ? '✅' : '❌';  
-      //   columns.push(result);
-      // })
-      // console.log(validationResult);
-      columns.push(
-        data.split('\n')
+      rows.push(
+        [title.toLowerCase(), ...data.split('\n')
         .slice(0, 8)
         .map((d) => {
           const status = d.slice(-6);
-          return status === 'passed' ? '✅' : '❌';
-        })
+          return status === 'passed' ? checkmark : crossmark;
+        })]
       );
+
 
       fs.writeFileSync(path.resolve(OUT_DIR, filename), data)
     } catch (error) {
@@ -80,18 +75,10 @@ async function getData(draftItems) {
   }
 
   // Write CSV
-
-  // Add values to the first row of the CSV
-  let csv = headers.join(',') + '\n';
-
-  // Add each row of validations to the CSV
-  for (let i = 0; i < columns[0].length; i++) {
-    let row = [];
-    for (let j = 0; j < columns.length; j++) {
-      row.push(columns[j][i]);
-    }
-    csv += row.join(',') + '\n';
-  }
+  let csv = '';
+  rows.forEach(row => {
+    csv += row.join(',') + '\n'
+  });
 
   fs.writeFileSync(path.resolve(OUT_DIR, 'summary.csv'), csv);
 
