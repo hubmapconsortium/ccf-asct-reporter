9e67e6d3c398613e8bb67b1fa837a859a921e31f
Abhay Rajde <abrajde@iu.edu>
2023-04-12 13:15:44 -0400

    Revert "added spreadsheet of all validation for summary"
    This reverts commit 8fbdf445686060ea2895e5e5432d6d70761030a9.

--- asctb-api/scripts/generate-draft-validation-reports.js
+++ asctb-api/scripts/generate-draft-validation-reports.js
@@ -3,17 +3,6 @@ const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
 
-// sheets imports
-const {google} = require('googleapis');
-const sheets = google.sheets('v4');
-const drive = google.drive('v3');
-
-// sheets authenticate
-const auth = new google.auth.GoogleAuth({
-  scopes: ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive'],
-  keyFile: '/Users/abhaybharatrajde/Documents/ASCT+B/ccf-asct-reporter/asctb-api/scripts/credentials.json',
-});
-
 let OUT_DIR = 'reports';
 let API_ENDPOINT = 'https://mmpyikxkcp.us-east-2.awsapprunner.com';
 if (process.argv.length > 2) {
@@ -51,8 +40,6 @@ async function getData(draftItems) {
   "Unmapped Data found?",
   "Bad Column found?"]];
 
-  const sheetData = [];
-
   for (let i = 0; i < draftItems.length; i++) {
     const item = draftItems[i];
     const filename = `${item.value}.txt`;
@@ -79,14 +66,6 @@ async function getData(draftItems) {
         })]
       );
 
-      // Google sheets
-      const sheetRows = [];
-      data.split('\n').map((line) => sheetRows.push([line]));
-      sheetData.push({
-        title: item.value,
-        data: sheetRows,
-      });
-
 
       fs.writeFileSync(path.resolve(OUT_DIR, filename), data)
     } catch (error) {
@@ -94,9 +73,6 @@ async function getData(draftItems) {
       process.exit(-1);
     }
   }
-
-  // Create google sheet
-  createSpreadsheet(sheetData);
 
   // Write CSV
   let csv = '';
@@ -107,72 +83,5 @@ async function getData(draftItems) {
   fs.writeFileSync(path.resolve(OUT_DIR, 'summary.csv'), csv);
 
 }
-
-
-async function createSpreadsheet(array) {
-  const authClient = await auth.getClient();
-
-  // Create a new spreadsheet
-  const res = await sheets.spreadsheets.create({
-    auth: authClient,
-    resource: {
-      properties: {
-        title: 'My Spreadsheet',
-      },
-    },
-  });
-
-  const spreadsheetId = res.data.spreadsheetId;
-
-
-  await drive.permissions.create({
-    auth: authClient,
-    fileId: spreadsheetId,
-    requestBody: {
-      role: 'reader',
-      type: 'anyone',
-    },
-  });
-
-
-  // Loop through each item in the array and create a new sheet for it
-  for (let i = 0; i < array.length; i++) {
-    const item = array[i];
-
-    // Add a new sheet to the spreadsheet
-    const addSheetRes = await sheets.spreadsheets.batchUpdate({
-      auth: authClient,
-      spreadsheetId: spreadsheetId,
-      resource: {
-        requests: [
-          {
-            addSheet: {
-              properties: {
-                title: item.title,
-              },
-            },
-          },
-        ],
-      },
-    });
-
-    const sheetId = addSheetRes.data.replies[0].addSheet.properties.sheetId;
-
-    // Write the data to the sheet
-    const writeRes = await sheets.spreadsheets.values.update({
-      auth: authClient,
-      spreadsheetId: spreadsheetId,
-      range: `${item.title}!A1`,
-      valueInputOption: 'RAW',
-      resource: {
-        values: item.data,
-      },
-    });
-  }
-
-  console.log(`Spreadsheet created with ID: ${spreadsheetId}`);
-  console.log(`Link to the spreadsheet: https://docs.google.com/spreadsheets/d/${spreadsheetId}/edit#gid=0`)
-}
-
 
 run();
