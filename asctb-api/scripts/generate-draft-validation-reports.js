172bcc82e988aa8ece4c61b5ee673a12495757e9
Bruce W. Herr II <bherr@indiana.edu>
2023-04-12 15:48:37 -0400

    Update validation report to generate xlsx

--- asctb-api/scripts/generate-draft-validation-reports.js
+++ asctb-api/scripts/generate-draft-validation-reports.js
@@ -1,24 +1,25 @@
-const sheetConfig = require('../../projects/v2/src/assets/sheet-config.json');
-const axios = require('axios');
-const fs = require('fs');
-const path = require('path');
+const sheetConfig = require("../../projects/v2/src/assets/sheet-config.json");
+const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
+const XLSX = require("xlsx");
 
-let OUT_DIR = 'reports';
-let API_ENDPOINT = 'https://mmpyikxkcp.us-east-2.awsapprunner.com';
+let XLSX_FILE = "validation-report.xlsx";
+let API_ENDPOINT = "https://mmpyikxkcp.us-east-2.awsapprunner.com";
 if (process.argv.length > 2) {
-  OUT_DIR = process.argv[2];
+  XLSX_FILE = process.argv[2];
 }
 if (process.argv.length > 3) {
   API_ENDPOINT = process.argv[3];
 }
 
 async function run() {
-  const allDraftItems= [];
+  const allDraftItems = [];
 
   sheetConfig.forEach((item) => {
     const version = item.version;
     if (version) {
-      const versionWithDraft = version.find((v) => v.value.includes('DRAFT'));
+      const versionWithDraft = version.find((v) => v.value.includes("DRAFT"));
       if (versionWithDraft) {
         allDraftItems.push(versionWithDraft);
       }
@@ -29,59 +30,66 @@ async function run() {
 }
 
 async function getData(draftItems) {
-  const checkmark = '\u2705';
-  const crossmark = '\u274C';
-  const rows = [["","Invalid CSV file?",
-  "Unmapped Metadata found?",
-  "Invalid Header found?",
-  "Missing Header Value found?",
-  "Invalid Character found?",
-  "Missing Uberon or CL IDs?",
-  "Unmapped Data found?",
-  "Bad Column found?"]];
+  const checkmark = "\u2705";
+  const crossmark = "\u274C";
+  const rows = [
+    [
+      "",
+      "Invalid CSV file?",
+      "Unmapped Metadata found?",
+      "Invalid Header found?",
+      "Missing Header Value found?",
+      "Invalid Character found?",
+      "Missing Uberon or CL IDs?",
+      "Unmapped Data found?",
+      "Bad Column found?",
+    ],
+  ];
+  const workbook = XLSX.utils.book_new();
+  const worksheets = {};
 
   for (let i = 0; i < draftItems.length; i++) {
     const item = draftItems[i];
-    const filename = `${item.value}.txt`;
     const sheetId = item.sheetId;
     const gid = item.gid;
-    
     const apiUrl = `${API_ENDPOINT}/v2/csv?csvUrl=https%3A%2F%2Fdocs.google.com%2Fspreadsheets%2Fd%2F${sheetId}%2Fedit%23gid%3D${gid}&output=validate`;
 
+    const filetitle = item.value.split("v1.")[0].split(/[_-]/);
+    const title = filetitle.join(" ").trim().toLowerCase();
+
     try {
-      const response = await axios.get(apiUrl, {
-        headers: {
-          'Content-Type': 'text/plain'
-        }
-      });
+      const response = await axios.get(apiUrl);
       const data = await response.data;
-      const filetitle = item.value.split('v1.')[0].split(/[_-]/);
-      const title = filetitle.join(' ');  
-      rows.push(
-        [title.toLowerCase(), ...data.split('\n')
-        .slice(0, 8)
-        .map((d) => {
-          const status = d.slice(-6);
-          return status === 'passed' ? checkmark : crossmark;
-        })]
-      );
 
+      rows.push([
+        title,
+        ...data
+          .split("\n")
+          .slice(0, 8)
+          .map((d) => {
+            const status = d.slice(-6);
+            return status === "passed" ? checkmark : crossmark;
+          }),
+      ]);
 
-      fs.writeFileSync(path.resolve(OUT_DIR, filename), data)
+      worksheets[title] = data.split("\n").map((n) => [n]);
     } catch (error) {
-      console.log('error: ', error);
-      process.exit(-1);
+      worksheets[title] = [["Invalid CSV file? failed"]];
+      console.log(
+        "Invalid CSV!",
+        item.value,
+        `https://docs.google.com/spreadsheets/d/${sheetId}/edit#gid=${gid}`
+      );
     }
   }
 
-  // Write CSV
-  let csv = '';
-  rows.forEach(row => {
-    csv += row.join(',') + '\n'
-  });
-
-  fs.writeFileSync(path.resolve(OUT_DIR, 'summary.csv'), csv);
-
+  const summarySheet = XLSX.utils.aoa_to_sheet(rows);
+  XLSX.utils.book_append_sheet(workbook, summarySheet, "Summary");
+  for (const [title, data] of Object.entries(worksheets)) {
+    const individualSheet = XLSX.utils.aoa_to_sheet(data);
+    XLSX.utils.book_append_sheet(workbook, individualSheet, title);
+  }
+  XLSX.writeFile(workbook, XLSX_FILE, { compression: true });
 }
 
 run();
