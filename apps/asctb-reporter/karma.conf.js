3e483a04fe4697c94b7f5f7d1c3a9dbb7ebf2318
Daniel Bolin <axbolin@iu.edu>
2024-03-18 13:51:03 -0400

    Update configs

--- apps/asctb-reporter/karma.conf.js
+++ apps/asctb-reporter/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/ccf-asct-reporter'),
+      dir: require('path').join(__dirname, '../../coverage/ccf-asct-reporter'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true,
       thresholds: {
@@ -30,7 +30,7 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    browsers: ['ChromeHeadless'],
+    browsers: ['ChromeHeadless' + (require('is-wsl') ? 'WSL' : '')],
     singleRun: true,
 
     customLaunchers: {
